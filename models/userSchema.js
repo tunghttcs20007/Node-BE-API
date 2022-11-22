8d302a1f5ca270b0f21aabb58e4c0e423401c9e0
tung.huynh <tunghttcs2007@fpt.edu.vn>
2022-11-22 17:07:30 +0700

    Fix: update user collection schema

--- models/userSchema.js
+++ models/userSchema.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema(
 			type: Array,
 			default: [],
 		},
-		address: String,
+		address: {},
 		wishlist: [{ type: ObjectId, ref: 'Product' }],
 	},
 	{ timestamps: true }
