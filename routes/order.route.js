b17ee8ab5c16a3bfad8dcf1e914521309d5a77f7
tung.huynh <tunghttcs20007@fpt.edu.vn>
2022-11-29 13:52:39 +0700

    Chore: remove redundant middleware check import

--- routes/order.route.js
+++ routes/order.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 /** Import Middlewars Function - Auth Controller */
-const { authVerifyToken, verifyAdminRole } = require('../middlewares/auth.middleware');
+const { authVerifyToken } = require('../middlewares/auth.middleware');
 
 /** Import Sub Category Controllers - CRUD */
 const {
